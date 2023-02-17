0b796f346621ff8d2c1e3d9fcf76257788b47eba
shusuke919 <sh091965@gmail.com>
2023-02-17 18:27:09 +0900

    first

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      こんにちわ
+    </div>
+  )
+}
+
+export default page
---
+++ my-app/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  // nextjs13の機能を使えるようにする
+  // https://zenn.dev/jtakahashi64/articles/a9d2ae3285ceb6
+  experimental: {
+    appDir: true,
+  }
+}
